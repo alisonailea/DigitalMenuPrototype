9f59818e30cb8623bc1f555c26bb452ea46e792b
Alison Stump <alison@alisonstump.com>
2014-02-17 10:57:05 -0800

    Issue #10: added Entrees, Desserts, and Drinks to JSON
    Also fixed a bug in the JSON link list that I made.
    
    Signed-off-by: Alison Stump <alison@alisonstump.com>

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -122,52 +122,25 @@ angular.module('myApp.controllers', []).
   controller('EntreeCtrl', function ($scope) {
     /* Data - this should be eventually pulled out to it's own JSON file 
        and called through an Angular service */
-    $scope.menuList = [
-      {'name': 'Lasagna Classico', 'imgUrl': 'img/appetizer/Mushrooms.png',
-       'description': 'Layers of pasta, meat sauce and mozzarella, ricotta, parmesan and romano cheese.',
-       'cost': '15.99'},
-      {'name': 'Eggplant Parmigiana', 'imgUrl': 'img/appetizer/Bread.png',
-       'description': 'Lightly breaded eggplant, fried and topped with marinara sauce, mozzarella and parmesan cheese. Served with spaghetti.',
-       'cost': '14.49'},
-      {'name': 'Fettuccine Alfredo', 'imgUrl': 'img/appetizer/Antipasto.png',
-       'description': 'Parmesan cream sauce with a hint of garlic, served over fettuccine.',
-       'cost': '14.49'},
-    ];
+    $scope.menuList = $scope.menu.entrees;
+
     $scope.updatePage('Entrees');
   }).
   /* Logic specific to the Dessert page. */
   controller('DessertCtrl', function ($scope) {
     /* Data - this should be eventually pulled out to it's own JSON file 
        and called through an Angular service */
-    $scope.menuList = [
-      {'name': 'Tiramisu', 'imgUrl': 'img/appetizer/Mushrooms.png',
-       'description': 'The classic Italian dessert. A layer of creamy custard set atop espresso-soaked ladyfingers.',
-       'cost': '7.29'},
-      {'name': 'Chocolate Mousse Cake', 'imgUrl': 'img/appetizer/Bread.png',
-       'description': 'Rich chocolate brownie topped with a layer of creamy chocolate mousse.',
-       'cost': '5.59'},
-      {'name': 'Lemon Cream Cake', 'imgUrl': 'img/appetizer/Antipasto.png',
-       'description': 'Delicate white cake and lemon cream filling with a vanilla crumb topping.',
-       'cost': '7.29'},
-    ];
-    $scope.updatePage('Dessert');
+    $scope.menuList = $scope.menu.desserts;
+
+    $scope.updatePage('Desserts');
   }).
   /* Logic specific to the Drink page. */
   controller('DrinkCtrl', function ($scope) {
     /* Data - this should be eventually pulled out to it's own JSON file 
        and called through an Angular service */
-    $scope.menuList = [
-      {'name': 'Pomegranate Gimlet', 'imgUrl': 'img/appetizer/Mushrooms.png',
-       'description': 'LetherbeeGin, Pomegranate Maracschino, Lime.',
-       'cost': '11.00'},
-      {'name': 'Classic Bellini', 'imgUrl': 'img/appetizer/Bread.png',
-       'description': 'White Peach, Prosecco.',
-       'cost': '11.00'},
-      {'name': 'Sicilian Mule', 'imgUrl': 'img/appetizer/Antipasto.png',
-       'description': 'Averna, Cynar, Lemon, Ginger Beer.',
-       'cost': '11.00'},
-    ];
-    $scope.updatePage('Drink');
+    $scope.menuList = $scope.menu.drinks;
+
+    $scope.updatePage('Drinks');
   }).
   /* Logic specific to the Revew your Order page. */
   controller('ReviewOrderCtrl', function ($scope) {
--- routes/api.js
+++ routes/api.js
@@ -13,8 +13,8 @@ exports.navigation = function (req, res) {
 		navigation: [
 			{'name': 'Appetizers', 'location': 'appetizer'},
 			{'name': 'Entrees', 'location': 'entrees'},
-			{'name': 'Dessert', 'location': 'dessert'},
-			{'name': 'Drink', 'location': 'drink'}
+			{'name': 'Dessert', 'location': 'desserts'},
+			{'name': 'Drink', 'location': 'drinks'}
 		]
 	});
 };
@@ -44,22 +44,62 @@ exports.menu = function (req, res) {
 			],
 			'entrees': [
 				{
-					'name': 'Entree 1',
+					'name': 'Lasagna Classico',
 					'imgUrl': 'img/appetizer/Mushrooms.png',
-					'description': 'Nunc pretium lectus augue, eget tincidunt arcu suscipit et. Donec vitae nisl in ante condimentum sollicitudin vel id elit.',
-					'cost': '6.00'
+					'description': 'Layers of pasta, meat sauce and mozzarella, ricotta, parmesan and romano cheese.',
+					'cost': '15.99'
 				},
 				{
-					'name': 'Entree 2',
+					'name': 'Eggplant Parmigiana',
 					'imgUrl': 'img/appetizer/Bread.png',
-					'description': 'Nunc pretium lectus augue, eget tincidunt arcu suscipit et. Donec vitae nisl in ante condimentum sollicitudin vel id elit.',
-					'cost': '5.50'
+					'description': 'Lightly breaded eggplant, fried and topped with marinara sauce, mozzarella and parmesan cheese. Served with spaghetti.',
+					'cost': '14.49'
 				},
 				{
-					'name': 'Entree 3',
+					'name': 'Fettuccine Alfredo',
 					'imgUrl': 'img/appetizer/Antipasto.png',
-					'description': 'Nunc pretium lectus augue, eget tincidunt arcu suscipit et. Donec vitae nisl in ante condimentum sollicitudin vel id elit.',
-					'cost': '5.00'
+					'description': 'Parmesan cream sauce with a hint of garlic, served over fettuccine.',
+					'cost': '14.49'
+				}
+			],
+			'desserts': [
+				{
+					'name': 'Tiramisu',
+					'imgUrl': 'img/appetizer/Mushrooms.png',
+					'description': 'The classic Italian dessert. A layer of creamy custard set atop espresso-soaked ladyfingers.',
+					'cost': '7.29'
+				},
+				{
+					'name': 'Chocolate Mousse Cake',
+					'imgUrl': 'img/appetizer/Bread.png',
+					'description': 'Rich chocolate brownie topped with a layer of creamy chocolate mousse.',
+					'cost': '5.59'
+				},
+				{
+					'name': 'Lemon Cream Cake',
+					'imgUrl': 'img/appetizer/Antipasto.png',
+					'description': 'Delicate white cake and lemon cream filling with a vanilla crumb topping.',
+					'cost': '7.29'
+				}
+			],
+			'drinks': [
+				{
+					'name': 'Pomegranate Gimlet',
+					'imgUrl': 'img/appetizer/Mushrooms.png',
+					'description': 'LetherbeeGin, Pomegranate Maracschino, Lime.',
+					'cost': '11.00'
+				},
+				{
+					'name': 'Classic Bellini',
+					'imgUrl': 'img/appetizer/Bread.png',
+					'description': 'White Peach, Prosecco.',
+					'cost': '11.00'
+				},
+				{
+					'name': 'Sicilian Mule',
+					'imgUrl': 'img/appetizer/Antipasto.png',
+					'description': 'Averna, Cynar, Lemon, Ginger Beer.',
+					'cost': '11.00'
 				}
 			]
 		}}
